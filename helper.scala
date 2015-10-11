eb601407ec99173441e3963fe381132719e4d8d8
Fishman <donghp1979@139.com>
2015-10-11 23:21:25 +0800

    commit akka sample

--- helper.scala
+++ helper.scala
@@ -1,40 +1,25 @@
-package helper {
-  import java.util.Calendar
-  import java.util.Date
-  
-  class DateHelper (val number: Int) {
+import akka.actor.Actor
 
-    // def days(when: String): Date = {
-    //   var date = Calendar.getInstance()
-    //   when match {
-    //     case DateHelper.ago => date.add(Calendar.DAY_OF_MONTH, -number)
-    //     case DateHelper.from_now => date.add(Calendar.DAY_OF_MONTH, number)
-    //     case _ => date
-    //   }
-    //   date.getTime()
-    // }
+case class PING()
+case class PONG()
 
-    def days = new FromHelper(this)
-  }
-
-  class FromHelper (val date:DateHelper) {
-    def from = new NowHelper(this)
-  }
-
-  class NowHelper (from:FromHelper) {
-    def now = {
-      var date = Calendar.getInstance()
-      date.add(Calendar.DAY_OF_MONTH, from.date.number)
-      date.getTime()
-    }
-  }
-
-  object DateHelper {
-    val ago = "ago"
-    val from_now = "from_now"
-    val fuck = "fuck"
-    implicit def convertInt2Date(number: Int) = new DateHelper(number)
-    //implicit def convertDate2FromHelper(date: Date) = new FromHelper(date)
+class PingPongActor extends Actor {
+  import context._
+  var count = 0
+  def receive: Receive = {
+    case PING =>
+      println("PING" + count)
+      count = count + 1
+      Thread.sleep(1000)
+      self ! PONG
+      become {
+        case PONG =>
+          println("PONG"+count)
+          count = count + 1
+          Thread.sleep(1000)
+          self ! PING
+          unbecome()
+      }
+      //if(count > 10) context.stop(self)
   }
 }
-
--- test.scala
+++ test.scala
@@ -1,16 +1,13 @@
-import helper.DateHelper._
+import akka.actor.ActorSystem
+import akka.actor.Props
 
-object Sample {
-  import java.util.Date
+object MyActorSystem {
 
   def main(args: Array[String]): Unit = {
-
-    // val twoDayAgo = 2 days ago
-    // val twoDayAfter = 2 days
-    // val twoDayFuck = 2 days fuck
-    // println("two days ago is " + twoDayAgo)
-    // println("two days after is " + twoDayAfter)
-    // println("two days fuck is " + twoDayFuck)
-    println("FUCK " + (2 days))
+    val _system = ActorSystem("BecomeUnbecome")
+    val pingPongActor = _system.actorOf(Props[PingPongActor])
+    pingPongActor ! PING
+    Thread.sleep(20000)
+    _system.shutdown
   }
 }
